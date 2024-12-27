0427d3bebdc08a8261c1ccb87a11ab7c9398b7ac
vodailuong2510 <vodailuong2510@gmail.com>
2024-12-27 22:43:26 +0700

    new base line

--- train.py
+++ train.py
@@ -1,24 +1,48 @@
-from Source.models import bert_finetune
-from Source.preprocessing import get_dataset, tokenize_data, get_tokenizer
+from QA.utils import load_dataset
+from QA.preprocessing import preprocessing
+from QA.models import bert_model
+from transformers import DefaultDataCollator
+from tf_keras.callbacks import ModelCheckpoint
 
 if __name__ == "__main__":
-    train_data = get_dataset("./data/train.csv")
-    valid_data = get_dataset("./data/val.csv")
+    dataset = load_dataset("./data")
 
-    model_name = "vinai/phobert-base"
+    batch_size = 16
+    learning_rate = 2e-5
+    num_epochs = 3
+    num_warmup_steps = 0
+    model_name = "bert-base-multilingual-cased"
 
-    train_tokenized_data = tokenize_data(train_data, model_name)
-    valid_tokenized_data = tokenize_data(valid_data, model_name)
+    tokenized_dataset = dataset.map(lambda examples: preprocessing(examples, model_name=model_name), batched=True, remove_columns=dataset["train"].column_names)
 
-    tokenizer = get_tokenizer(model_name)
+    data_collator = DefaultDataCollator(return_tensors="tf")
 
-    model = bert_finetune(model_name, output_dir= "./results", evaluation_strategy= "epoch", 
-                learning_rate= 2e-5, per_device_train_batch_size= 16, 
-                num_train_epochs= 3, weight_decay= 0.01, save_steps= 10, 
-                logging_dir= "./results/logs", train_tokenized_data= train_tokenized_data, 
-                valid_tokenized_data= valid_tokenized_data, tokenizer= tokenizer)
+
+    total_train_steps = (len(tokenized_dataset["train"]) // batch_size)
+
+    model = bert_model(batch_size=batch_size, learning_rate=learning_rate, 
+                       num_warmup_steps=0, num_train_steps=total_train_steps, model_name_or_path=model_name)
     
-    model.train()
+    tf_train_set = model.prepare_tf_dataset(
+        tokenized_dataset["train"],
+        shuffle=True,
+        batch_size=batch_size,
+        collate_fn=data_collator,
+    )
 
-    model.save_pretrained("./qa_model")
-    tokenizer.save_pretrained("./qa_model")
+    tf_valid_set = model.prepare_tf_dataset(
+        tokenized_dataset["val"],
+        shuffle=False,
+        batch_size=batch_size,
+        collate_fn=data_collator,
+    )
+
+    checkpoint_callback = ModelCheckpoint(
+        filepath="./results/best_model.keras",  
+        monitor='val_loss',
+        save_best_only=True,  
+        save_freq='epoch',                               
+        verbose=1                                  
+    )           
+
+    model.fit(x=tf_train_set, validation_data=tf_valid_set, epochs=3, callbacks=[checkpoint_callback], verbose=1)
