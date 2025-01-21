7a16700ae583a6658ad5e7116d037d38bd0b1e0b
vodailuong2510 <vodailuong2510@gmail.com>
2025-01-21 10:03:31 +0700

    fix bug class, compute em metric on train and valid set

--- QA/__init__.py
+++ QA/__init__.py
@@ -2,4 +2,3 @@ from QA.utils import *
 from QA.preprocessing import *
 from QA.models import *
 from QA.evaluate import *
-from QA.classes import *
--- QA/models.py
+++ QA/models.py
@@ -1,11 +1,12 @@
 from transformers import create_optimizer
 from transformers import AutoModelForQuestionAnswering, TrainingArguments, Trainer
 from QA.preprocessing import get_tokenizer
-from QA.evaluate import compute_em
+import numpy as np
+import evaluate
 
 def train_bert_model(learning_rate:float= 2e-5, weight_decay:float= 0.01,
                batch_size:int= 16, num_train_epochs:int= 3, model_name_or_path:str= "vinai/phobert-base", 
-               data_collator=None, eval_dataset= None, train_dataset=None) -> None:
+               data_collator=None, eval_dataset= None, train_dataset=None, save_path:str="./results/saved_model") -> None:
 
     tokenizer = get_tokenizer(model_name_or_path)
 
@@ -22,6 +23,32 @@ def train_bert_model(learning_rate:float= 2e-5, weight_decay:float= 0.01,
         push_to_hub=False,
     )
 
+    def compute_metrics(eval_pred):
+        metric = evaluate.load("exact_match")
+        logits, labels = eval_pred
+
+        print(labels)
+
+        start_logits, end_logits = logits
+        start_predictions = np.argmax(start_logits, axis=-1)
+        end_predictions = np.argmax(end_logits, axis=-1)
+
+        predictions = []
+        references = []
+        for i in range(len(labels['id'])): 
+            prediction_text = tokenizer.decode(
+                labels['input_ids'][i][start_predictions[i]:end_predictions[i] + 1],
+                skip_special_tokens=True,
+                clean_up_tokenization_spaces=True,
+            )
+            reference_text = labels['answers']['text'][i][0] 
+
+            predictions.append({"id": labels['id'][i], "prediction_text": prediction_text})
+            references.append({"id": labels['id'][i], "answers": {"text": [reference_text]}})
+        
+        results = metric.compute(predictions=predictions, references=references)
+        return results
+
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -29,7 +56,7 @@ def train_bert_model(learning_rate:float= 2e-5, weight_decay:float= 0.01,
         eval_dataset=eval_dataset,
         processing_class=tokenizer,
         data_collator=data_collator,
-        compute_metrics=compute_em,
+        compute_metrics=compute_metrics,
     )
 
     trainer.train()
