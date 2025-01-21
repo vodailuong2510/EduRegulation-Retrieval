217a63ceee4a9b8d5a27b6c535ba9f6187f4d8af
vodailuong2510 <vodailuong2510@gmail.com>
2025-01-21 12:49:25 +0700

    delete computing em metric on training process

--- QA/models.py
+++ QA/models.py
@@ -1,8 +1,5 @@
-from transformers import create_optimizer
 from transformers import AutoModelForQuestionAnswering, TrainingArguments, Trainer
 from QA.preprocessing import get_tokenizer
-import numpy as np
-import evaluate
 
 def train_bert_model(learning_rate:float= 2e-5, weight_decay:float= 0.01,
                batch_size:int= 16, num_train_epochs:int= 3, model_name_or_path:str= "vinai/phobert-base", 
@@ -24,32 +21,6 @@ def train_bert_model(learning_rate:float= 2e-5, weight_decay:float= 0.01,
         report_to = "none",
     )
 
-    def compute_metrics(eval_pred):
-        metric = evaluate.load("exact_match")
-        logits, labels = eval_pred
-
-        print(labels)
-
-        start_logits, end_logits = logits
-        start_predictions = np.argmax(start_logits, axis=-1)
-        end_predictions = np.argmax(end_logits, axis=-1)
-
-        predictions = []
-        references = []
-        for i in range(len(labels['id'])): 
-            prediction_text = tokenizer.decode(
-                labels['input_ids'][i][start_predictions[i]:end_predictions[i] + 1],
-                skip_special_tokens=True,
-                clean_up_tokenization_spaces=True,
-            )
-            reference_text = labels['answers']['text'][i][0] 
-
-            predictions.append({"id": labels['id'][i], "prediction_text": prediction_text})
-            references.append({"id": labels['id'][i], "answers": {"text": [reference_text]}})
-        
-        results = metric.compute(predictions=predictions, references=references)
-        return results
-
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -57,7 +28,6 @@ def train_bert_model(learning_rate:float= 2e-5, weight_decay:float= 0.01,
         eval_dataset=eval_dataset,
         processing_class=tokenizer,
         data_collator=data_collator,
-        compute_metrics=compute_metrics,
     )
 
     trainer.train()
