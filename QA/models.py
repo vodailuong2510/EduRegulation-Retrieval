61a71358844d38a3b3fe15cdf02ef3cb26fc4188
vodailuong2510 <vodailuong2510@gmail.com>
2025-01-21 09:46:26 +0700

    delete callback, change framework, fix offset mapping

--- QA/models.py
+++ QA/models.py
@@ -1,17 +1,36 @@
 from transformers import create_optimizer
-from transformers import TFAutoModelForQuestionAnswering
+from transformers import AutoModelForQuestionAnswering, TrainingArguments, Trainer
+from QA.preprocessing import get_tokenizer
+from QA.evaluate import compute_em
 
-def bert_model(learning_rate:float= 2e-5,
-               num_warmup_steps:int= 0, num_train_steps:int= -1, model_name_or_path:str= "vinai/phobert-base"):
+def train_bert_model(learning_rate:float= 2e-5, weight_decay:float= 0.01,
+               batch_size:int= 16, num_train_epochs:int= 3, model_name_or_path:str= "vinai/phobert-base", 
+               data_collator=None, eval_dataset= None, train_dataset=None) -> None:
 
-    optimizer, scheduler = create_optimizer(
-        init_lr=learning_rate,
-        num_warmup_steps=num_warmup_steps,
-        num_train_steps=num_train_steps,
+    tokenizer = get_tokenizer(model_name_or_path)
+
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name_or_path)
+    
+    training_args = TrainingArguments(
+        output_dir="",
+        eval_strategy="epoch",
+        learning_rate=learning_rate,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        num_train_epochs=num_train_epochs,
+        weight_decay=weight_decay,
+        push_to_hub=False,
     )
 
-    model = TFAutoModelForQuestionAnswering.from_pretrained(model_name_or_path)
-    model.compile(optimizer=optimizer)
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        processing_class=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_em,
+    )
 
-    return model
+    trainer.train()
 
--- train.py
+++ train.py
@@ -1,70 +1,23 @@
-import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices=false'
-
 from QA.utils import load_dataset
-from QA.preprocessing import preprocessing, get_tokenizer
-from QA.models import bert_model
-from QA.classes import ExactMatchCallback
+from QA.preprocessing import preprocessing
+from QA.models import train_bert_model
 from transformers import DefaultDataCollator
-from transformers.keras_callbacks import PushToHubCallback
 
 if __name__ == "__main__":
     dataset = load_dataset("./data")
 
-    batch_size = 16
     learning_rate = 2e-5
+    weight_decay= 0.01
+    batch_size = 16
     num_epochs = 3
-    num_warmup_steps = 0
     model_name = "xlm-roberta-base"
+    data_collator = DefaultDataCollator()
 
     tokenized_dataset = dataset.map(lambda examples: preprocessing(examples, model_name=model_name), batched=True, remove_columns=dataset["train"].column_names)
-
-    data_collator = DefaultDataCollator(return_tensors="tf")
-
-    total_train_steps = (len(tokenized_dataset["train"]) // batch_size)
-    model = bert_model(learning_rate=learning_rate, num_warmup_steps=0, 
-                       num_train_steps=total_train_steps, model_name_or_path=model_name)
     
     small_train_dataset = tokenized_dataset["train"].select(range(100)) 
-    small_val_dataset = tokenized_dataset["val"].select(range(20)) 
+    small_eval_dataset = tokenized_dataset["val"].select(range(20)) 
 
-    tf_train_set = model.prepare_tf_dataset(
-        small_train_dataset,
-        shuffle=True,
-        batch_size=batch_size,
-        collate_fn=data_collator,
-    )
-
-    tf_valid_set = model.prepare_tf_dataset(
-        small_val_dataset,
-        shuffle=False,
-        batch_size=batch_size,
-        collate_fn=data_collator,
-    )   
-    
-    # tf_train_set = model.prepare_tf_dataset(
-    #     tokenized_dataset["train"],
-    #     shuffle=True,
-    #     batch_size=batch_size,
-    #     collate_fn=data_collator,
-    # )
-
-    # tf_valid_set = model.prepare_tf_dataset(
-    #     tokenized_dataset["val"],
-    #     shuffle=False,
-    #     batch_size=batch_size,
-    #     collate_fn=data_collator,
-    # )      
-
-    callback = PushToHubCallback(
-        output_dir="./results/saved_model",
-        tokenizer=get_tokenizer(model_name)
-    )   
-
-    callback_em = ExactMatchCallback(
-        valid_dataset=tf_valid_set, 
-        tokenizer=get_tokenizer(model_name)
-    )
-
-    model.fit(x=tf_train_set, validation_data=tf_valid_set, epochs=num_epochs, callbacks=[callback_em, callback], verbose=1)
+    train_bert_model(learning_rate=learning_rate, weight_decay=weight_decay, 
+                     batch_size=batch_size, num_train_epochs=num_epochs, model_name_or_path=model_name, 
+                     data_collator=data_collator, eval_dataset=small_eval_dataset, train_dataset=small_train_dataset)
