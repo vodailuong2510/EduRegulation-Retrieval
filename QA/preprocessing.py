d6930bddf10bbc524f022d074ae896727f3fbbb3
vodailuong2510 <vodailuong2510@gmail.com>
2025-01-21 09:13:54 +0700

    fix offset mapping

--- QA/preprocessing.py
+++ QA/preprocessing.py
@@ -9,7 +9,7 @@ def create_offset_mapping(text, tokenizer):
     current_position = 0
 
     for token in tokens:
-        token = token.replace("▁", "")  # Xóa ký tự đặc biệt của PhoBERT
+        token = token.replace("▁", "") 
         start_index = text.find(token, current_position)
         end_index = start_index + len(token)
         offset_mapping.append((start_index, end_index))
@@ -21,19 +21,22 @@ def preprocessing(examples, model_name: str = "vinai/phobert-base"):
     tokenizer = get_tokenizer(model_name)
 
     questions = [q.strip() for q in examples["question"]]
+
     inputs = tokenizer(
         questions,
         examples["context"],
         max_length=400,
         truncation="only_second",
         padding="max_length",
+        return_offsets_mapping=True,
     )
 
-    offset_mappings = []
-    for context in examples["context"]:
-        offset_mapping = create_offset_mapping(context, tokenizer)
-        offset_mappings.append(offset_mapping)
+    # offset_mappings = []
+    # for context in examples["context"]:
+    #     offset_mapping = create_offset_mapping(context, tokenizer)
+    #     offset_mappings.append(offset_mapping)
 
+    offset_mappings = inputs.pop("offset_mapping")
     answers = examples["extractive answer"]
     start_positions = []
     end_positions = []
@@ -42,22 +45,29 @@ def preprocessing(examples, model_name: str = "vinai/phobert-base"):
         answer = answers[i]
         start_char = answer["answer_start"][0]
         end_char = answer["answer_start"][0] + len(answer["text"][0])
-        
-        context_offset = offset
-        if context_offset[0][0] > end_char or context_offset[-1][1] < start_char:
+        sequence_ids = inputs.sequence_ids(i)
+
+        idx = 0
+        while sequence_ids[idx] != 1:
+            idx += 1
+        context_start = idx
+        while sequence_ids[idx] == 1:
+            idx += 1
+        context_end = idx - 1
+
+        if offset[context_start][0] > end_char or offset[context_end][1] < start_char:
             start_positions.append(0)
             end_positions.append(0)
         else:
-            start_idx = next(
-                (idx for idx, (start, _) in enumerate(context_offset) if start <= start_char < _),
-                None
-            )
-            end_idx = next(
-                (idx for idx, (_, end) in enumerate(context_offset) if start_char < end >= end_char),
-                None
-            )
-            start_positions.append(start_idx if start_idx is not None else 0)
-            end_positions.append(end_idx if end_idx is not None else 0)
+            idx = context_start
+            while idx <= context_end and offset[idx][0] <= start_char:
+                idx += 1
+            start_positions.append(idx - 1)
+
+            idx = context_end
+            while idx >= context_start and offset[idx][1] >= end_char:
+                idx -= 1
+            end_positions.append(idx + 1)
 
     inputs["start_positions"] = start_positions
     inputs["end_positions"] = end_positions
