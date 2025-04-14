f075352654d08f708498c8bacffe7b46f27048cd
Luongdayne <vodailuong2510@gmail.com>
2025-04-14 21:56:56 +0700

    Refactor response handling in backend.py, update frontend.html for better user experience, and modify MongoDB configuration in docker-compose file

--- QA/response.py
+++ QA/response.py
@@ -6,7 +6,8 @@ def infer(question, context, model_name_or_path= "vodailuong2510/MLops"):
 
     return result
 
-def reply(question, context, model_path="vodailuong2510/MLops"):
+async def reply(question, context, model_path="vodailuong2510/MLops"):
     result = infer(question=question, context=context, model_name_or_path=model_path)
 
-    return result
+    for char in result['answer']:
+        yield char
