84d7716b7313e7a28ffcec300de71387e4a3e1a8
Luongdayne <vodailuong2510@gmail.com>
2025-04-14 20:30:17 +0700

    Refactor response.py, update frontend.html, and remove unused frontend2.html; add logo image

--- QA/response.py
+++ QA/response.py
@@ -1,5 +1,4 @@
 from transformers import pipeline
-from .retrieve import retrieve_document
 
 def infer(question, context, model_name_or_path= "vodailuong2510/MLops"):
     qa_pipeline = pipeline("question-answering", model=model_name_or_path, tokenizer=model_name_or_path)
@@ -11,10 +10,3 @@ def reply(question, context, model_path="vodailuong2510/MLops"):
     result = infer(question=question, context=context, model_name_or_path=model_path)
 
     return result
-
-if __name__ == "__main__":
-    query = "Sinh viên chưa hết thời gian tối đa hoàn thành khóa học quy định tại Điều 6 của Quy chế này, đã hoàn thành các học phần trong chương trình đào tạo có nguyện vọng xin thôi học theo diện này thì phải làm gì?"
-    context= retrieve_document(query= query)
-
-    answer = reply(query, context, model_path="vodailuong2510/MLops")
-    print("Answer:", answer)
