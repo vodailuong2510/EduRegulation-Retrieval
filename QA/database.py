cec799ecaf11251689c635e74da0b281b5c6f7bc
Luongdayne <vodailuong2510@gmail.com>
2025-04-14 19:05:37 +0700

    Add database functionality and update README with project details
    - Implement MongoDB integration with functions to save, retrieve, and delete messages.
    - Enhance Dockerfile to install necessary packages and set up the environment.
    - Create DVC pipeline configuration for data processing, training, and evaluation stages.
    - Revise README to provide comprehensive project overview and setup instructions.

---
+++ QA/database.py
@@ -0,0 +1,35 @@
+from pymongo import MongoClient
+
+def get_mongo_collection(Mongo_Key):
+    client = MongoClient(Mongo_Key)
+    db = client["chat"] 
+    messages_collection = db["messages"] 
+
+    return messages_collection
+
+
+def save_message(messages_collection, chat_id: str, sender: str, content: str):
+    message = {
+        "chat_id": chat_id,
+        "sender": sender,
+        "content": content,
+    }
+    messages_collection.insert_one(message)
+
+def get_last_messages(messages_collection, chat_id: str, limit: int = 10):
+    messages = messages_collection.find({"chat_id": chat_id}) \
+        .sort("timestamp", -1) \
+        .limit(limit)
+
+    return [{"role": "user" if msg["sender"] == "user" else "assistant", "content": msg["content"]} for msg in messages]
+
+def delete_all_messages(messages_collection, chat_id):
+    result = messages_collection.delete_many({"chat_id": chat_id})
+    return result.deleted_count
+
+if __name__ == "__main__":
+    import os
+    from dotenv import load_dotenv, find_dotenv
+    load_dotenv(find_dotenv())
+
+    print("Deleted:", delete_all_messages(get_mongo_collection(os.getenv("MONGO_URI")), chat_id= 1))
